5ee5b7c02b444d7ab0dd34e9ca0795466a471f02
WWork777 <REPetitorAv1to@ya.ru>
2025-12-06 22:10:17 +0300

    fixes imports

--- components/Bootstrap/Bootstrap.js
+++ components/Bootstrap/Bootstrap.js
@@ -1,17 +1,17 @@
-"use client"
+'use client';
 import { useEffect } from 'react';
-import './bootstrap.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Bootstrap = () => {
-    useEffect(() => {
-        import('bootstrap/dist/js/bootstrap.bundle.min.js')
-            .then((module) => {
-                console.log('Bootstrap JS loaded', module);
-            })
-            .catch((err) => {
-                console.error('Error loading Bootstrap JS:', err);
-            });
-    }, []);
-    return null
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+      .then((module) => {
+        console.log('Bootstrap JS loaded', module);
+      })
+      .catch((err) => {
+        console.error('Error loading Bootstrap JS:', err);
+      });
+  }, []);
+  return null;
 };
 
 export default Bootstrap;
